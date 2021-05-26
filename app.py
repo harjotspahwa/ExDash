e997cb16e7cd51bf9c1d534da4e2180a6e8e42fa
Harjot Pahwa <23222646+vortexeye@users.noreply.github.com>
2021-05-26 13:50:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print('In App')
-    X_train, y_train, X_test, y_test = titanic_embarked()
-    model = RandomForestClassifier(n_estimators=50, max_depth=10).fit(X_train, y_train)
+#     X_train, y_train, X_test, y_test = titanic_embarked()
+#     model = RandomForestClassifier(n_estimators=50, max_depth=10).fit(X_train, y_train)
 
-    explainer = ClassifierExplainer(model, X_test, y_test, 
-                                    cats=['Sex', 'Deck'], 
-                                    descriptions=feature_descriptions,
-                                    labels=['Queenstown', 'Southampton', 'Cherbourg'])
+#     explainer = ClassifierExplainer(model, X_test, y_test, 
+#                                     cats=['Sex', 'Deck'], 
+#                                     descriptions=feature_descriptions,
+#                                     labels=['Queenstown', 'Southampton', 'Cherbourg'])
 
-    db = ExplainerDashboard(explainer)
+#     db = ExplainerDashboard(explainer)
     print('About to run')
-    db.run(mode="external")
+#     db.run(mode="external")
     print('Ran??')
