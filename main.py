e1c1e27a85797bbaae37ed2d69d04a40ef93f0ba
Harjot Pahwa <23222646+vortexeye@users.noreply.github.com>
2021-05-26 10:41:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from sklearn.ensemble import RandomForestClassifier
+
+from explainerdashboard import ClassifierExplainer, ExplainerDashboard
+from explainerdashboard.datasets import titanic_embarked, feature_descriptions
+
+X_train, y_train, X_test, y_test = titanic_embarked()
+model = RandomForestClassifier(n_estimators=50, max_depth=10).fit(X_train, y_train)
+
+explainer = ClassifierExplainer(model, X_test, y_test, 
+                                cats=['Sex', 'Deck'], 
+                                descriptions=feature_descriptions,
+                                labels=['Queenstown', 'Southampton', 'Cherbourg'])
+
+db = ExplainerDashboard(explainer)
+
+if __name__ == '__main__':
+    db.run(debug=True)
